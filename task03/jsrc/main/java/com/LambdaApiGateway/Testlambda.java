af56e59287dc5776b35bda4723466230e703f745
Ilgiz Shakirov <ilgiz_shakirov@epam.com>
2024-05-04 18:26:44 +0500

    Task03 attempt 2

--- task03/jsrc/main/java/com/LambdaApiGateway/Testlambda.java
+++ task03/jsrc/main/java/com/LambdaApiGateway/Testlambda.java
@@ -8,7 +8,7 @@ import com.syndicate.deployment.model.RetentionSetting;
 import java.util.HashMap;
 import java.util.Map;
 
-@LambdaHandler(lambdaName = "TestLambda",
+@LambdaHandler(lambdaName = "hello_world",
 	roleName = "TestLambda-role",
 	isPublishVersion = true,
 	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
