f0e84df3be7fe7fc8d711dec127fcd490a759619
Ilgiz Shakirov <ilgiz_shakirov@epam.com>
2024-05-14 17:18:47 +0500

    Task08 attempt2

--- task08/jsrc/main/java/com/task08/Apihandler.java
+++ task08/jsrc/main/java/com/task08/Apihandler.java
@@ -2,36 +2,39 @@ package com.task08;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.google.gson.Gson;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
+import com.syndicate.deployment.annotations.lambda.LambdaLayer;
 import com.syndicate.deployment.annotations.lambda.LambdaUrlConfig;
+import com.syndicate.deployment.model.Architecture;
+import com.syndicate.deployment.model.ArtifactExtension;
+import com.syndicate.deployment.model.DeploymentRuntime;
 import com.syndicate.deployment.model.RetentionSetting;
-
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 
 @LambdaHandler(lambdaName = "api_handler",
 	roleName = "ApiHandler-role",
 	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
+@LambdaLayer(
+		layerName = "open-meteo-layer",
+		libraries = {"lib/gson-2.10.1.jar"},
+		runtime = DeploymentRuntime.JAVA11,
+		architectures = {Architecture.ARM64},
+		artifactExtension = ArtifactExtension.ZIP
+)
+
 @LambdaUrlConfig
 public class Apihandler implements RequestHandler<Object, String> {
 
 	public String handleRequest(Object request, Context context) {
-		var currentWeatherRequest = HttpRequest.newBuilder()
-				.uri(URI.create("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m"))
-				.GET()
-				.build();
+		System.out.println("Handling request: " + request);
 
-		HttpResponse<String> response;
-		try {
-			response = HttpClient.newBuilder()
-					.build()
-					.send(currentWeatherRequest, HttpResponse.BodyHandlers.ofString());
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-		return response.body();
+		var gson = new Gson();
+		System.out.println("Gson = " + gson);
+
+		var response = OpenMeteoClient.getCurrentWeather();
+		System.out.println("Weater response = " + response);
+
+		return response;
 	}
 }
---
+++ task08/jsrc/main/java/com/task08/OpenMeteoClient.java
@@ -0,0 +1,27 @@
+package com.task08;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class OpenMeteoClient {
+
+    public static String getCurrentWeather() {
+        var currentWeatherRequest = HttpRequest.newBuilder()
+                .uri(URI.create("https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m"))
+                .GET()
+                .build();
+
+        HttpResponse<String> response;
+        try {
+            response = HttpClient.newBuilder()
+                    .build()
+                    .send(currentWeatherRequest, HttpResponse.BodyHandlers.ofString());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        return response.body();
+    }
+}
